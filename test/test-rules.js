90d6bec292e32068b1a47864bf839a807090cc1e
Sebastian Schürmann <sschuermann303@yahoo.de>
2010-10-03 22:28:10 +0200

    - added fuzzy rules aka boolean logic

---
+++ test/test-rules.js
@@ -0,0 +1,9 @@
+require('./common');
+assert.ok(typeof rules != 'undefined');
+
+// assert.ok(rules.and(0.1, 0.2) == 0.1);
+assert.ok(rules.or(0.1, 0.2)== 0.2);
+assert.ok(rules.not(0.1) == 0.9);
+
+
+
