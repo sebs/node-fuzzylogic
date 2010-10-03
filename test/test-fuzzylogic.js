07f22c3a63b3f1709eaddb8b72e48d94437eb2ce
Sebastian Schürmann <sschuermann303@yahoo.de>
2010-10-03 19:01:48 +0200

    - making the makefile work - adding test stuff to the test file

--- test/test-fuzzylogic.js
+++ test/test-fuzzylogic.js
@@ -1,2 +1,11 @@
 require('./common');
 
+var resGrade = fuzzylogic.grade(3,0,1);
+assert.ok(resGrade == 1);
+
+var resReverseGrade = fuzzylogic.reverseGrade(3,0,1);
+assert.ok(resReverseGrade == 0);
+
+
+
+
