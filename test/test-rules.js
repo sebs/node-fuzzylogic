9651a5c873ff97ddaddec58b5c549be3ad7b640f
Sebastian Schürmann <sschuermann303@yahoo.de>
2010-10-14 16:05:11 +0200

    - re-added test for fuzzy rules

--- test/test-rules.js
+++ test/test-rules.js
@@ -1,7 +1,7 @@
 require('./common');
 assert.ok(typeof rules != 'undefined');
 
-// assert.ok(rules.and(0.1, 0.2) == 0.1);
+assert.ok(rules.and(0.1, 0.2) == 0.1);
 assert.ok(rules.or(0.1, 0.2)== 0.2);
 assert.ok(rules.not(0.1) == 0.9);
 
