3239975ec5c768dd760a97e48c22c7698d260028
Sebastian Schürmann <sschuermann303@yahoo.de>
2010-10-15 21:26:33 +0200

    - fuzzy rules with callbacks - there is no stopping me now

--- test/test-rules.js
+++ test/test-rules.js
@@ -1,8 +1,17 @@
 require('./common');
 assert.ok(typeof rules != 'undefined');
 
-assert.ok(rules.and(0.1, 0.2) == 0.1);
-assert.ok(rules.or(0.1, 0.2)== 0.2);
+var cbValue = 'none';
+var cbA = function() {
+    cbValue = 'a';
+}
+
+var cbB = function() {
+    cbValue = 'b';
+}
+
+assert.ok(rules.and(0.1, 0.2, cbA, cbB) == 0.1);
+assert.ok(rules.or(0.1, 0.2, cbA, cbB)== 0.2);
 assert.ok(rules.not(0.1) == 0.9);
 
 
