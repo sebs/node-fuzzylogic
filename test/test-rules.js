1c459843e15a05135555af2d4d9118c414e46ae6
stream <sebs@2xs.org>
2015-04-30 12:14:27 +0200

    Promises for the logic

--- test/test-rules.js
+++ test/test-rules.js
@@ -2,22 +2,21 @@ var assert = require("assert");
 var rules = require("../index").rules;
 describe('Rules', function () {
   it('should execute the right callback for and', function (done) {
-    var res = rules.and(0.1, 0.2, function () {
+    var res = rules.and(0.1, 0.2).then(function(res) {
+      assert.equal(res, 0.1);
       done();
-    }, function () {
-      assert.ok(false);
     });
-    assert.equal(res, 0.1);
   });
   it('should execute the right callback for or', function (done) {
-    var res = rules.or(0.1, 0.2, function () {
-      assert.ok(false);
-    }, function () {
+    var res = rules.or(0.1, 0.2).then(function(res) {
+      assert.equal(res, 0.2);
       done();
     });
-    assert.equal(res, 0.2);
   });
-  it('should return the right value on not', function() {
-    assert.equal(rules.not(0.1), 0.9);
+  it('should return the right value on not', function(done) {
+    rules.not(0.1).then(function(res) {
+      assert.equal(res, 0.9);
+      done();
+    })
   });
 });
