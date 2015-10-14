039216752e0687a1597da8b2b18bf88e4344a363
sebs <sebs@2xs.org>
2015-10-14 10:27:34 +0200

    Improved tests to make example work

--- test/test-logic.js
+++ test/test-logic.js
@@ -1,14 +1,7 @@
 var assert = require("assert");
 var fuzzylogic = require("../lib/fuzzylogic");
 
-
-
-
 describe('Logic', function(){
-  it('interface check', function() {
-    assert.equal(typeof fuzzylogic.grade, 'function');
-  });
-
   it('grade', function(){
     var resGrade = fuzzylogic.grade(3,0,1);
     assert.equal(resGrade, 1);
@@ -24,5 +17,33 @@ describe('Logic', function(){
   it('trapezoid', function(){
     var resTrapezoid = fuzzylogic.trapezoid(3,0,1,2,3);
     assert.equal(resTrapezoid, 0);
+  });
+
+  describe('demo works', function() {
+    var attackCalc = function(threat) {
+      var probabNoAttack          = fuzzylogic.triangle(threat, 0, 20, 40);
+      var probabNormalAttack      = fuzzylogic.trapezoid(threat, 20, 30, 90, 100);
+      var probabEnragedAttack     = fuzzylogic.grade(threat, 90, 100);
+      return {
+        no: probabNoAttack,
+        normal: probabEnragedAttack,
+        enraged: probabEnragedAttack
+      }
+    }
+
+    it('calculates no attack for 10', function() {
+      var res = attackCalc(10);
+      assert.equal(res.no, 0.5);
+    });
+
+    it('calculates normal attack for 10', function() {
+      var res = attackCalc(10);
+      assert.equal(res.normal, 0);
+    });
+
+    it('calculates enraged attack for 10', function() {
+      var res = attackCalc(10);
+      assert.equal(res.enraged, 0);
+    });
   });
 });
